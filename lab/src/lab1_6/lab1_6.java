6218eb3631f2bafff49afc5f1f08c8e882e53aca
Phumipat Wongdaw <Phumipat56110@gmail.com>
2025-11-22 19:44:47 +0700

    lab1_6

--- lab/src/lab1_6/lab1_6.java
+++ lab/src/lab1_6/lab1_6.java
@@ -1,24 +1,26 @@
 package lab1_6;
-import java.util.Scanner;	//	เพิ่มฟังค์ชั่นรับค่าจากผู้ใช้เข้ามา
+import java.util.Scanner;
 
 public class lab1_6 {
 
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);	// รับค่า input จากผู้ใช้งาน
-		int number001 = input.nextInt();	// ให้ number001 = ค่าที่กรอกเข้าไป	
-		input.close();	// ปิดการรับค่า
+		int number001;
+		System.out.println("Enter number :");
+		Scanner input = new Scanner(System.in);
+		number001 = input.nextInt();
+		input.close();
 		
-		System.out.println(number001 + " x 1 = " + number001 * 1);	//	ให้เเสดงค่า ตัวเลขที่กรอกมา x 1 = ผลคูณของตัวเลขที่เรากรอกมา
-		System.out.println(number001 + " x 2 = " + number001 * 2);	//	ให้เเสดงค่า ตัวเลขที่กรอกมา x 2 = ผลคูณของตัวเลขที่เรากรอกมา
-		System.out.println(number001 + " x 3 = " + number001 * 3);	//	ให้เเสดงค่า ตัวเลขที่กรอกมา x 3 = ผลคูณของตัวเลขที่เรากรอกมา
-		System.out.println(number001 + " x 4 = " + number001 * 4);	//	ให้เเสดงค่า ตัวเลขที่กรอกมา x 4 = ผลคูณของตัวเลขที่เรากรอกมา
-		System.out.println(number001 + " x 5 = " + number001 * 5);	//	ให้เเสดงค่า ตัวเลขที่กรอกมา x 5 = ผลคูณของตัวเลขที่เรากรอกมา
-		System.out.println(number001 + " x 6 = " + number001 * 6);	//	ให้เเสดงค่า ตัวเลขที่กรอกมา x 6 = ผลคูณของตัวเลขที่เรากรอกมา
-		System.out.println(number001 + " x 7 = " + number001 * 7);	//	ให้เเสดงค่า ตัวเลขที่กรอกมา x 7 = ผลคูณของตัวเลขที่เรากรอกมา
-		System.out.println(number001 + " x 8 = " + number001 * 8);	//	ให้เเสดงค่า ตัวเลขที่กรอกมา x 8 = ผลคูณของตัวเลขที่เรากรอกมา
-		System.out.println(number001 + " x 9 = " + number001 * 9);	//	ให้เเสดงค่า ตัวเลขที่กรอกมา x 9 = ผลคูณของตัวเลขที่เรากรอกมา
-		System.out.println(number001 + " x 10 = " + number001 * 10);	//	ให้เเสดงค่า ตัวเลขที่กรอกมา x 10 = ผลคูณของตัวเลขที่เรากรอกมา
-		System.out.println(number001 + " x 11 = " + number001 * 11);	//	ให้เเสดงค่า ตัวเลขที่กรอกมา x 11 = ผลคูณของตัวเลขที่เรากรอกมา
-		System.out.println(number001 + " x 12 = " + number001 * 12);	//	ให้เเสดงค่า ตัวเลขที่กรอกมา x 12 = ผลคูณของตัวเลขที่เรากรอกมา
+		System.out.println(number001 + " x 1 = " + number001 * 1);
+		System.out.println(number001 + " x 2 = " + number001 * 2);
+		System.out.println(number001 + " x 3 = " + number001 * 3);
+		System.out.println(number001 + " x 4 = " + number001 * 4);
+		System.out.println(number001 + " x 5 = " + number001 * 5);
+		System.out.println(number001 + " x 6 = " + number001 * 6);
+		System.out.println(number001 + " x 7 = " + number001 * 7);
+		System.out.println(number001 + " x 8 = " + number001 * 8);
+		System.out.println(number001 + " x 9 = " + number001 * 9);
+		System.out.println(number001 + " x 10 = " + number001 * 10);
+		System.out.println(number001 + " x 11 = " + number001 * 11);
+		System.out.println(number001 + " x 12 = " + number001 * 12);
 	}
 }
