f709668cb727d6b56ccf4872829c11d287223eda
Phumipat Wongdaw <Phumipat56110@gmail.com>
2025-11-22 19:06:57 +0700

    lab1_1

--- lab/src/lab1_1/lab1_1.java
+++ lab/src/lab1_1/lab1_1.java
@@ -1,13 +1,14 @@
 package lab1_1;
-import java.util.Scanner;	//	import รับค่าจากผู้ใช้งาน
+import java.util.Scanner;
 
 public class lab1_1 {
 
 	public static void main(String[] args) {
+		System.out.println("Enter number : ");
 		Scanner input = new Scanner(System.in);
-		int number001 = input.nextInt();	//	ให้ค่าที่กรอกเข้ามาตัวที่ 1 เป็น number001
-		int number002 = input.nextInt();	//	ให้ค่าที่กรอกเข้ามาตัวที่ 2 เป็น number002
-		System.out.println(number001 + number002);	//	แสดงผลลัพธ์ของ number001 + number002
-		input.close();	//	ปิดการรับค่าจากผู้ใช้งาน
-	}	//	End main
-}	// End Class
+		int number001 = input.nextInt();
+		int number002 = input.nextInt();
+		System.out.println(number001 + number002);
+		input.close();
+	}
+}
