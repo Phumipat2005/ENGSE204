37320b9acd606fee8c8323c461a5f504657ebdf0
Phumipat Wongdaw <Phumipat56110@gmail.com>
2025-11-22 18:34:28 +0700

    lab1_4

--- lab/src/lab1_4/lab1_4.java
+++ lab/src/lab1_4/lab1_4.java
@@ -1,22 +1,22 @@
 package lab1_4;
-import java.util.Scanner;
+import java.util.Scanner;	// import รับค่าจากผู้ใช้งาน
 
 public class lab1_4 {
-
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		int number1 = input.nextInt();
-		input.close();
-		if (number1 >= 80 ) {
-			System.out.println("A");
-		} else if (number1 >= 70) {
-			System.out.println("B");
-		} else if (number1 >= 60 ) {
-			System.out.println("C");
-		} else if (number1 >= 50 ) {
-			System.out.println("D");
-		} else if (number1 < 50 ) {
-			System.out.println("F");
-		} 
-	}
-}
+		int number001 = input.nextInt();	//	ให้ number001 เท่ากับค่าที่กรอกเข้ามา
+		input.close();	//	ปิดการรับค่าจากผู้ใช้
+		
+		if (number001 >= 80 ) {	// Start if เมื่อ number001 มากกว่าหรือเท่ากับ 80
+			System.out.println("A");	//	ให้เเสดงค่าว่า A
+		} else if (number001 >= 70) {	//	เมื่อ number001 มากกว่าหรือเท่ากับ 70
+			System.out.println("B");	//	ให้เเสดงค่าว่า B
+		} else if (number001 >= 60 ) {	//	เมื่อ number001 มากกว่าหรือเท่ากับ 60
+			System.out.println("C");	//	ให้เเสดงค่าว่า C
+		} else if (number001 >= 50 ) {	//	เมื่อ number001 มากกว่าหรือเท่ากับ 50
+			System.out.println("D");	//	ให้เเสดงค่าว่า D
+		} else if (number001 < 50 ) {	//	เมื่อ number001 น้อยกว่า 50
+			System.out.println("F");	//	ให้เเสดงค่าว่า F
+		}	//	End if
+	}	// End main
+}	//	End class
