f26162c9939ca72af0342a51dd3ea28411146a3f
Phumipat Wongdaw <Phumipat56110@gmail.com>
2025-11-22 18:27:08 +0700

    lab1_3

--- lab/src/lab1_3/lab1_3.java
+++ lab/src/lab1_3/lab1_3.java
@@ -1,14 +1,14 @@
 package lab1_3;
-import java.util.Scanner;
+import java.util.Scanner;	//	import รับค่าจากผู้ใช้งาน
 
 public class lab1_3 {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		int number1 = input.nextInt();
-		if (number1 % 2 == 0) {
-			System.out.println("Even");
+		int number001 = input.nextInt();	//	ให้ number001 เท่ากับค่าที่กรอกเข้ามา
+		if (number001 % 2 == 0) {	//	Start if
+			System.out.println("Even");	//	เเสดงคำว่า Even
 		} else
-			System.out.println("Odd");
-	}
-}
+			System.out.println("Odd");	//	เเสดงค่าว่า Odd
+	}	// 	End main
+}	// End class
